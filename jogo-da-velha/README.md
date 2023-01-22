9c5b490e1834d684a404a65e149e36fa3f047a4e
Cristiano Ledur <cristianoledur98@gmail.com>
2023-01-22 02:02:50 -0300

    Upload jogo da velha + .gitignore

---
+++ jogo-da-velha/README.md
@@ -0,0 +1,25 @@
+# Jogo da velha
+
+## Proposta do projeto
+
+Fazer um jogo da velha usando flexbox para organizar os elementos na tela. Utilizando eventos do DOM para detectar quando um quadrado é clicado e alterar a cor para indicar jogada.
+
+<img src="assets/imgProjeto.png" alt="Imagem do projeto desktop" style="width: 600px">
+
+> Projeto feito utilizando HTML, CSS e JS com o intuito de aperfeiçoar meus conhecimentos em flex-box e interações com o DOM.
+
+## 🚀 Acessando a interface
+
+Para acessar o projeto, basta [clicar aqui](spiteful-steam.surge.sh)
+
+> Projeto disponivel no link acima até 16/05/2023.
+
+## 📝 Licença
+
+Esse projeto está sob licença. Veja o arquivo [LICENÇA](LICENSE.md) para mais detalhes.
+
+## Status
+
+🎯 Concluído
+
+[⬆ Voltar ao topo](#Grid-Layout)<br>
---
+++ jogo-da-velha/main.js
@@ -0,0 +1,66 @@
+var listPlayer1 = [];
+var listPlayer2 = [];
+var players = [];
+let currentPlayer = 1;
+let resultados = [['1','2','3'],['4','5','6'],['7','8','9'],['1','4','7'],['2','5','8'],['3','6','9'],['1','5','9'],['3','5','7']];
+let condicao = false;
+let winner = "";
+
+var parent = document.querySelector("#box");
+parent.addEventListener("click", (event) => {
+  if(event.target.matches(".box__table")) {
+    var i = event.target.getAttribute("data-index");
+    if((currentPlayer === 1) && (listPlayer1.indexOf(i) === -1)) {
+      listPlayer1.push(i);
+      var img = document.createElement('img');
+      img.setAttribute("class", "imgChild");
+      img.src = "assets/circulo.png";
+      img.style.width = "50%";
+      event.target.appendChild(img);
+      validaLista(listPlayer1);
+      currentPlayer = 2;
+    } else if ((currentPlayer === 2) && (listPlayer1.indexOf(i) === -1)) {
+      listPlayer2.push(i);
+      var img = document.createElement('img');
+      img.setAttribute("class", "imgChild");
+      img.src = "assets/x.png";
+      img.style.width = "50%";
+      event.target.appendChild(img);
+      validaLista(listPlayer2);
+      currentPlayer = 1;
+    }
+  }
+});
+
+function start() {
+  var player1 = document.getElementById("player1").value;
+  var player2 = document.getElementById("player2").value;
+  document.getElementById("info__players").style.display = "none";
+  document.getElementById("table").style.display = "flex";
+  players.push(player2);
+  players.push(player1);
+}
+
+function validaLista(list) {
+  resultados.forEach(sequence => {
+    let temp = [...sequence];
+    list.forEach(item => {
+      let index = temp.indexOf(item);
+      if (index !== -1) {
+        temp.splice(index, 1);
+      }
+    });
+    if (temp.length === 0 ) {
+      condicao = true;
+      if (currentPlayer === 2 ) {
+        winner = players[0];
+      } else {
+        winner = players[1];
+      }
+    }
+  });
+  if(condicao) {
+    document.getElementById("result").style.display = "flex";
+    document.getElementById("winner").textContent = winner;
+  }
+}
